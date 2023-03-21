090fcfe4d6be2d37ba821c01127afa2cc6039d4c
yoshito1125 <yoshito5211@gmail.com>
2023-03-22 01:53:24 +0900

    Webサーバの立ち上げ(動的ページ)

--- webrick.rb
+++ webrick.rb
@@ -1,11 +1,19 @@
  # webrick.rb
 require 'webrick'
 
-server = WEBrick::HTTPServer.new({ 
+server = WEBrick::HTTPServer.new({
   :DocumentRoot => './',
   :BindAddress => '127.0.0.1',
   :Port => 8000
 })
+
+server.mount_proc("/time") do |req, res|
+  # レスポンス内容を出力
+  body = "<html><body>#{DateTime.now}</body></html>"
+  res.status = 200
+  res['Content-Type'] = 'text/html'
+  res.body = body
+end
 
 trap(:INT){
     server.shutdown
