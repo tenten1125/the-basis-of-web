dd2213e8eddb578f9ede08532410c75a07b54694
yoshito1125 <yoshito5211@gmail.com>
2023-03-23 22:58:38 +0900

    フォーム編・課題2

--- webrick.rb
+++ webrick.rb
@@ -7,16 +7,42 @@ server = WEBrick::HTTPServer.new({
   :Port => 8000
 })
 
+trap(:INT){
+    server.shutdown
+}
+
 server.mount_proc("/time") do |req, res|
   # レスポンス内容を出力
-  body = "<html><body>#{DateTime.now}</body></html>"
+  body = "<html><body>\n"
+  body += "#{Time.new}"
+  body += "</body></html>\n"
   res.status = 200
   res['Content-Type'] = 'text/html'
   res.body = body
 end
 
-trap(:INT){
-    server.shutdown
-}
+server.mount_proc("/form_get") do |req, res|
+  # レスポンス内容を出力
+  body = "<html><head><meta charset='utf-8'></head><body>\n"
+  body += "クエリパラメータは#{req.query}です<br>\n"
+  body += "こんにちは#{req.query['username']}さん。"
+  body += "あなたの年齢は#{req.query['age']}ですね"
+  body += "</body></html>\n"
+  res.status = 200
+  res['Content-Type'] = 'text/html'
+  res.body = body
+end
+
+server.mount_proc("/form_post") do |req, res|
+  # レスポンス内容を出力
+  body = "<html><head><meta charset='utf-8'></head><body>\n"
+  body += "フォームデータは#{req.query}です<br>\n"
+  body += "こんにちは#{req.query['username']}さん。"
+  body += "あなたの年齢は#{req.query['age']}ですね"
+  body += "</body></html>\n"
+  res.status = 200
+  res['Content-Type'] = 'text/html'
+  res.body = body
+end
 
 server.start
